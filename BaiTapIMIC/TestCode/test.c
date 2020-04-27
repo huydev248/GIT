b7445427a617d4d5c06105c8093e815aba83a2ed
huy <shinehuy24@gmail.com>
2020-04-27 09:15:30 +0700

    .

--- BaiTapIMIC/TestCode/test.c
+++ BaiTapIMIC/TestCode/test.c
@@ -1,42 +1,35 @@
-﻿#include <stdio.h>
-#include <math.h>
+﻿#include<stdio.h>
 
-double Power_n(double x, long n);
-double qPower_n(double x, long n);
+//int DoDai(char* s);
 
 int main()
 {
-	double x = 0;
-	long n = 0;
-	printf("Nhap x va n: \r\n", x, n);
-	scanf("%f%d", &x, &n);
-
-	Power_n(x, n);
-	qPower_n(x, n);
+	//char* msg = "hello";
+	///*char msg[] = "hello";
+	//char* pc = msg;*/
+	//int length = 0;
+	//length = DoDai(msg);
+	//printf("Do dai chuoi la: %d", length);
+	//return 0;
+	int x = 10;
+	int* px = x;
+	printf("Gia tri cua x: %d", *px);
+	printf("Dia chi cua px: %d", px);
+	printf("Dia chi cua x: %d", &x);
+	return 0;
 }
 
-double Power_n(double x, long n)
-{
-	double result = 1;
-	while (n--)
-	{
-		result *= x;
-	}
-	return result;
-}
-
-double qPower_n(double x, long n)
-{
-	double result = 1;
-	while (n)
-	{
-		if (n % 2 == 1)
-		{
-			result *= x;
-		}
-		x *= x;
-		n /= 2;
-	}
-	return result;
-}
-
+//int DoDai(char* s)
+//{
+//	/*int t = 9999;*/
+//	int count = 0;
+//	for (int i = 0; *(s + i) != 0; i++)
+//	{
+//		/*if (*(s + i) == 0)
+//		{
+//			break;
+//		}*/
+//		count++;
+//	}
+//	return count;
+//}
