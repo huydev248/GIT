b53e394e27f48d3af53bac3b60abf77f7ee6b113
huy <shinehuy24@gmail.com>
2020-06-08 15:16:53 +0700

    ...

--- BaiTapIMIC/TestCode2/test2.cpp
+++ BaiTapIMIC/TestCode2/test2.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World";
+	ofstream file("test.txt");
+	char name[100];
+	cin.getline(name, 100);
+
+	file << name << endl;
+
+	file.close();
+
+	return 0;
 }
